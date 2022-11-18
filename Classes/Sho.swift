26bfab2bf2f13286df6c2aee9cf7eff90ffd90f0
Showaa-06 <115485438+Showaa-06@users.noreply.github.com>
2022-11-18 17:47:53 +0900

    Complete

---
+++ Classes/Sho.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+class curry {
+    var onio = 10
+    var carrots = 10
+    
+    func addmeats() {
+        print("add meat 20")
+        
+    }
+    
+    func addpotatoes() {
+        print("add potatoes 5")
+        
+        
+    }
+    
+
+}
