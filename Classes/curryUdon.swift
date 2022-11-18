9b5f8c0d1b6bb2209a57ed068aad8f743ee399eb
Showaa-06 <115485438+Showaa-06@users.noreply.github.com>
2022-11-18 17:48:16 +0900

    Complete

---
+++ Classes/curryUdon.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+class curryUdon: curry {
+    var addCurryroux = 4
+    var addWater = 1
+    
+    
+    
+    //コードの上書き
+    override func addmeats() {
+        print("add a lot of meat")
+    }
+    
+    
+    
+}
+
